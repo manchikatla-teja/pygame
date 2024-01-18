2ddaffe180e8bb923ec69ff7c15cb3f99ed7f90c
TejaManchikatla <cs21b031@iittp.ac.in>
2024-01-19 01:15:54 +0530

    decided a few characters, came up with dimensions to work on

--- test.py
+++ test.py
@@ -2,6 +2,25 @@
 import pygame
 from sys import exit
 from AllScenes import display_text
+from pytmx.util_pygame import load_pygame
+ 
+class Tile(pygame.sprite.Sprite):
+    def __init__(self,pos,surf,groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_rect(topleft = pos)
+
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+screen.fill('red')
+
+
+ 
+# for obj in tmx_data.objects:
+#     pos = obj.x,obj.y
+#     if obj.type in ('Building', 'Vegetation'):
+#         Tile(pos = pos, surf = obj.image, groups = sprite_group)
 
 def displayScene():
     arrayOfTexts = display_text("scene16", current_dialogue)
@@ -15,8 +34,6 @@ def displayScene():
     screen.blit(text_surf2, text_rect2)
     screen.blit(text_surf3, text_rect3)
 
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption('Runenr')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/PixelType.ttf', 56)
@@ -31,13 +48,29 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.MOUSEBUTTONUP:
+        if event.type == pygame.MOUSEBUTTONUP and event.button ==1: #1means left button of mouse
             current_dialogue+=3
 
-    
+#     tmx_data = load_pygame('yes.tmx')
+#     sprite_group = pygame.sprite.Group()
+
+ 
+# # cycle through all layers
+#     for layer in tmx_data.visible_layers:
+#     # if layer.name in ('Floor', 'Plants and rocks', 'Pipes')
+#         if hasattr(layer,'data'):
+#             for x,y,surf in layer.tiles():
+#                 pos = (x * 64 + 300, y * 64)
+#                 Tile(pos = pos, surf = surf, groups = sprite_group)
 
 
+#     sprite_group.draw(screen)
+    image = pygame.image.load("tiles/arjun.jpg").convert_alpha()
+    image.set_colorkey('black')
+    rect = image.get_rect()
+    screen.blit(image, rect)
     screen.blit(text_box, (20, 480))
+    
     displayScene()
     pygame.display.update()
     clock.tick(60)
